d2994af9ae4480a469afbf4b2f6b9b828676b69f
arun-r <arun_psg@yahoo.com>
2014-11-09 02:15:01 -0500

    Working version . With test on sphere
    1. Sphere test
    2. test implmentation with sphere.

--- geomboost.h
+++ geomboost.h
@@ -38,8 +38,6 @@ template <typename T>
 struct t_traits{
 	typedef BOOST_TYPEOF_TPL(boost::declval<T const&>().x()) real_t;
 
-	typedef T* TRef;
-
 	static real_t getx(const T& val){
 		return val.x();
 	}
@@ -227,6 +225,10 @@ public:
 	}
 
 	boundingbox():xmin(0), xmax(0), ymin(0), ymax(0), zmin(0), zmax(0){}
+
+	bool isvalid()const{
+		return xmin <= xmax && ymin <= ymax && zmin <= zmax;
+	}
 };
 
 
@@ -234,13 +236,12 @@ public:
 template <typename T>
 class closest_point_finder : public t_traits<T>{
 
+
 	std::vector<const T*> points;
 	static const unsigned int numdivs = 3;
-	std::vector<const T*> binpoints[numdivs+1][numdivs+1][numdivs+1];
+	std::vector<const T*> pointbins[numdivs+1][numdivs+1][numdivs+1];
 	boundingbox<T> bbox;
-	real_t binx;
-	real_t biny;
-	real_t binz;
+	real_t binsize;
 
 
 	struct DistComp{
@@ -251,31 +252,262 @@ class closest_point_finder : public t_traits<T>{
 		}
 	};
 
+	struct DistCompSafe{
+		const T& point;
+		DistCompSafe(const T& point):point(point){}
+		bool operator()(const T*& lhs, const T*& rhs){
+			if(lhs && rhs)
+				return squared_distance(*lhs, point) < squared_distance(*rhs, point);
+			else if(lhs)
+				return true;
+			else if(rhs)
+				return false;
+			else 
+				return &lhs < &rhs;
+		}
+	};
+
+	
+
+	inline const T* find_closest_in_range(const T& point, int xmin, int xmax, 
+		int ymin, int ymax, int zmin, int zmax)const{
+		const T* closepoint = NULL;
+		if(xmin <= xmax && ymin <= ymax && zmin <= zmax){
+			real_t sqmindist = -1;
+			DistComp distcomp(point);
+			for (int ii = xmin; ii <= xmax; ++ii){
+				for (int jj = ymin; jj <= ymax; ++jj){
+					for (int kk = zmin; kk <= zmax; ++kk){
+						const std::vector<const T*>& bin = pointbins[ii][jj][kk];
+						if( bin.size() > 0 ){
+							const T* binclosepoint = *(std::min_element(bin.begin(), bin.end(), distcomp));
+							real_t binsqdist = squared_distance(point, *binclosepoint);
+							if(sqmindist < 0 || sqmindist < binsqdist){
+								closepoint = binclosepoint;
+								sqmindist = binsqdist;
+							}
+						}
+					}
+				}
+			}
+
+		}
+		return closepoint;
+	}
+
+
+
+	const T* find_closest_at_radius(const T& point, unsigned int radius)const{
+		int xindx = std::max(std::min((int )((getx(point) - bbox.xmin)/binsize), (int)numdivs), (int )0);
+		int yindx = std::max(std::min((int )((gety(point) - bbox.ymin)/binsize), (int)numdivs), (int )0);
+		int zindx = std::max(std::min((int )((getz(point) - bbox.zmin)/binsize), (int)numdivs), (int )0);
+		
+		const int numpoints = 6+12+8;//6sides +12 edges + 8 corners of a cube/cuboid
+		const T* closepoints[numpoints];
+		int totalboxes = radius > 0 ? numpoints : 1;
+
+		/*int xx[2] = {std::max((int)(xindx - radius), (int)0 ), std::min((int)(xindx + radius), (int)numdivs)};
+		int yy[2] = {std::max((int)(yindx - radius), (int)0), std::min((int)(yindx + radius), (int)numdivs)};
+		int zz[2] = {std::max((int)(zindx - radius), (int)0), std::min((int)(zindx + radius), (int)numdivs)};
+
+		const int offset[numpoints][6] = {
+			//corners	
+			{0, 0, 0, 0, 0, 0},	{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0},	{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0},	{0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0},	{0, 0, 0, 0, 0, 0},
+			//sides
+			{1, -1, 1, -1, 0, 0}, {1, -1, 1, -1, 0, 0},
+			{1, -1, 0, 0, 1, -1}, {1, -1, 0, 0, 1, -1},
+			{0, 0, 1, -1, 1, -1}, {0, 0, 1, -1, 1, -1},
+			//edges
+			{1, -1, 0, 0, 0, 0}, {1, -1, 0, 0, 0, 0}, {1, -1, 0, 0, 0, 0}, {1, -1, 0, 0, 0, 0},
+			{0, 0, 1, -1, 0, 0}, {0, 0, 1, -1, 0, 0}, {0, 0, 1, -1, 0, 0}, {0, 0, 1, -1, 0, 0},
+			{0, 0, 0, 0, 1, -1}, {0, 0, 0, 0, 1, -1}, {0, 0, 0, 0, 1, -1}, {0, 0, 0, 0, 1, -1}
+		};
+
+		const int index[numpoints][6] = {
+			//corners	
+			{0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 1}, 
+			{0, 0, 1, 1, 0, 0}, {0, 0, 1, 1, 1, 1}, 
+			{1, 1, 0, 0, 0, 0}, {1, 1, 0, 0, 1, 1}, 
+			{1, 1, 1, 1, 0, 0}, {1, 1, 1, 1, 1, 1},
+			//sides
+			{0, 1, 0, 1, 0, 0},	{0, 1, 0, 1, 1, 1}, 
+			{0, 1, 0, 0, 0, 1},	{0, 1, 1, 1, 0, 1}, 
+			{0, 0, 0, 1, 0, 1},	{1, 1, 0, 1, 0, 1}, 
+			//edges
+			{0, 1, 0, 0, 0, 0},	{0, 1, 1, 1, 0, 0}, {0, 1, 0, 0, 1, 1}, {0, 1, 1, 1, 1, 1}, 
+			{0, 0, 0, 1, 0, 0}, {1, 1, 0, 1, 0, 0}, {0, 0, 0, 1, 1, 1}, {1, 1, 0, 1, 1, 1}, 
+			{0, 0, 0, 0, 0, 1}, {1, 1, 0, 0, 0, 1}, {0, 0, 1, 1, 0, 1}, {1, 1, 1, 1, 0, 1}
+		};
+
+		
+		for ( int tt = 0; tt < totalboxes; ++tt){
+
+			int xmin = xx[index[tt][0]] + offset[tt][0];
+			int xmax = xx[index[tt][1]] + offset[tt][1];
+			int ymin = yy[index[tt][2]] + offset[tt][2];
+			int ymax = yy[index[tt][3]] + offset[tt][3];
+			int zmin = zz[index[tt][4]] + offset[tt][4];
+			int zmax = zz[index[tt][5]] + offset[tt][5];
+			closepoints[tt] = NULL;
+			if(xmin <= xmax && ymin <= ymax && zmin <= zmax){
+				real_t sqmindist = -1;
+				DistComp distcomp(point);
+				for (int ii = xmin; ii <= xmax; ++ii){
+					for (int jj = ymin; jj <= ymax; ++jj){
+						for (int kk = zmin; kk <= zmax; ++kk){
+							const std::vector<const T*>& bin = pointbins[ii][jj][kk];
+							if( bin.size() > 0 ){
+								const T* binclosepoint = *(std::min_element(bin.begin(), bin.end(), distcomp));
+								real_t binsqdist = squared_distance(point, *binclosepoint);
+								if(sqmindist < 0 || sqmindist < binsqdist){
+									closepoints[tt] = binclosepoint;
+									sqmindist = binsqdist;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		*/
+		
+
+		int xmin = std::max((int)(xindx - radius), (int)0);
+		int xmax = std::min((int)(xindx + radius), (int)numdivs);
+		int ymin = std::max((int)(yindx - radius), (int)0);
+		int ymax = std::min((int)(yindx + radius), (int)numdivs);
+		int zmin = std::max((int)(zindx - radius), (int)0);
+		int zmax = std::min((int)(zindx + radius), (int)numdivs);
+
+
+		if (radius == 0){
+			return find_closest_in_range(point, xmin, xmax, ymin, ymax, zmin, zmax);
+		}
+
+		closepoints[0] = find_closest_in_range(point, xmin+1, xmax-1, ymin+1, ymax-1, zmin, zmin);
+		closepoints[1] = find_closest_in_range(point, xmin+1, xmax-1, ymin+1, ymax-1, zmax, zmax);
+		closepoints[2] = find_closest_in_range(point, xmin+1, xmax-1, ymin, ymin, zmin+1, zmax-1);
+		closepoints[3] = find_closest_in_range(point, xmin+1, xmax-1, ymax, ymax, zmin+1, zmax-1);
+		closepoints[4] = find_closest_in_range(point, xmin, xmin, ymin+1, ymax-1, zmin+1, zmax-1);
+		closepoints[5] = find_closest_in_range(point, xmax, xmax, ymin+1, ymax-1, zmin+1, zmax-1);
+			
+		
+		closepoints[6] = find_closest_in_range(point, xmin+1, xmax-1, ymin, ymin, zmin, zmin);
+		closepoints[7] = find_closest_in_range(point, xmin+1, xmax-1, ymax, ymax, zmin, zmin);
+		closepoints[8] = find_closest_in_range(point, xmin+1, xmax-1, ymin, ymin, zmax, zmax);
+		closepoints[9] = find_closest_in_range(point, xmin+1, xmax-1, ymax, ymax, zmax, zmax);
+			
+
+		closepoints[10] = find_closest_in_range(point, xmin, xmin, ymin+1, ymax-1, zmin, zmin);
+		closepoints[11] = find_closest_in_range(point, xmax, xmax, ymin+1, ymax-1, zmin, zmin);
+		closepoints[12] = find_closest_in_range(point, xmin, xmin, ymin+1, ymax-1, zmax, zmax);
+		closepoints[13] = find_closest_in_range(point, xmax, xmax, ymin+1, ymax-1, zmax, zmax);
+
+			 
+
+		closepoints[14] = find_closest_in_range(point, xmin, xmin, ymin, ymin, zmin+1, zmax-1);
+		closepoints[15] = find_closest_in_range(point, xmax, xmax, ymin, ymin, zmin+1, zmax-1);
+		closepoints[16] = find_closest_in_range(point, xmin, xmin, ymax, ymax, zmin+1, zmax-1);
+		closepoints[17] = find_closest_in_range(point, xmax, xmax, ymax, ymax, zmin+1, zmax-1);
+		
+		
+		closepoints[18] = find_closest_in_range(point, xmin, xmin, ymin, ymin, zmin, zmin);
+		closepoints[19] = find_closest_in_range(point, xmin, xmin, ymin, ymin, zmax, zmax);
+		closepoints[20] = find_closest_in_range(point, xmin, xmin, ymax, ymax, zmin, zmin);
+		closepoints[21] = find_closest_in_range(point, xmin, xmin, ymax, ymax, zmax, zmax);
+		closepoints[22] = find_closest_in_range(point, xmax, xmax, ymin, ymin, zmin, zmin);
+		closepoints[23] = find_closest_in_range(point, xmax, xmax, ymin, ymin, zmax, zmax);
+		closepoints[24] = find_closest_in_range(point, xmax, xmax, ymax, ymax, zmin, zmin);
+		closepoints[25] = find_closest_in_range(point, xmax, xmax, ymax, ymax, zmax, zmax);
+
+		DistCompSafe distcomp(point);
+		const T* closepoint = *(std::min_element(closepoints, closepoints + totalboxes, distcomp));
+		return closepoint;
+
+	}
+
 
 public:
 
 	void recalculate(){
 		bbox = boundingbox<T>(points.begin(), points.end());
-		binx = (bbox.xmax - bbox.xmin)/numdivs;
-		biny = (bbox.ymax - bbox.ymin)/numdivs;
-		binz = (bbox.zmax - bbox.zmin)/numdivs;
+		
+		binsize = std::max(std::max((bbox.xmax - bbox.xmin)/numdivs, (bbox.ymax - bbox.ymin)/numdivs), (bbox.zmax - bbox.zmin)/numdivs);
 		for(std::vector<const T*>::const_iterator iter = points.begin(); iter != points.end() ; ++iter){
-			unsigned int xindx = (unsigned int )((getx(**iter) - bbox.xmin)/binx);
-			unsigned int yindx = (unsigned int )((gety(**iter) - bbox.ymin)/biny);
-			unsigned int zindx = (unsigned int )((getz(**iter) - bbox.zmin)/binz);
-			binpoints[xindx][yindx][zindx].push_back(*iter);
+			unsigned int xindx = (unsigned int )((getx(**iter) - bbox.xmin)/binsize);
+			unsigned int yindx = (unsigned int )((gety(**iter) - bbox.ymin)/binsize);
+			unsigned int zindx = (unsigned int )((getz(**iter) - bbox.zmin)/binsize);
+			pointbins[xindx][yindx][zindx].push_back(*iter);
 		}
 	}
 
+
+
+	
 	
 	const T* find_closest(const T& point)const{
-		unsigned int xindx = std::max(std::min((unsigned int )((getx(point) - bbox.xmin)/binx), numdivs), (unsigned int )0);
-		unsigned int yindx = std::max(std::min((unsigned int )((gety(point) - bbox.ymin)/biny), numdivs), (unsigned int )0);
-		unsigned int zindx = std::max(std::min((unsigned int )((getz(point) - bbox.zmin)/binz), numdivs), (unsigned int )0);
+
+		if ( points.size() == 0 )
+			return NULL;
+		bool debugit = false;
+		//DWORD t1, t2, t3;
+		//t1 = GetTickCount();
+
+		unsigned int radius = 0;
+		const T* closepoint = NULL;
+		while(!closepoint) {
+			//loop in increasing radii until we find a close point.
+			closepoint = find_closest_at_radius(point, radius++);
+		}
+
+		real_t sqmindist1 = squared_distance(*closepoint, point);
+
+		int xindx = std::max(std::min((int )((getx(point) - bbox.xmin)/binsize), (int)numdivs), (int )0);
+		int yindx = std::max(std::min((int )((gety(point) - bbox.ymin)/binsize), (int)numdivs), (int )0);
+		int zindx = std::max(std::min((int )((getz(point) - bbox.zmin)/binsize), (int)numdivs), (int )0);
+
 		
-		DistComp distcomp(point);
-		const std::vector<const T*>& bin = binpoints[xindx][yindx][zindx];
-		return bin.size() == 0 ? NULL : *(std::min_element(bin.begin(), bin.end(), distcomp));
+		T centerpoint = t_traits<T>::makeT<T>( bbox.xmin + xindx*binsize + binsize/2, 
+						bbox.ymin + yindx*binsize + binsize/2, bbox.zmin + zindx*binsize + binsize/2 );
+		real_t sqmindist2 = squared_distance(*closepoint, centerpoint);
+		real_t maxradiusf = sqrt(std::max(sqmindist1, sqmindist2))/binsize;
+
+		unsigned int maxradius = (unsigned int)(maxradiusf) + (maxradiusf > floor(maxradiusf) ? 1 : 0);
+		while (  radius < maxradius ){
+
+			const T* another_closepoint = find_closest_at_radius(point, radius++);
+			if (squared_distance(*another_closepoint, point) < sqmindist1){
+				closepoint = another_closepoint;
+				sqmindist1 = squared_distance(*closepoint, point);
+				sqmindist2 = squared_distance(*closepoint, centerpoint);
+
+				real_t maxradiusf = sqrt(std::max(sqmindist1, sqmindist2))/binsize;
+				maxradius = (unsigned int)(maxradiusf) + (maxradiusf > floor(maxradiusf) ? 1 : 0);
+			}
+			//debugit = true;
+		}
+		
+		//t2 =  GetTickCount();
+		//if(debugit)
+			//cout << "POINT " << point << " TIME = " << t2-t1 << endl;
+		return closepoint;
+	}
+
+	const T* find_closest_linear(const T& point)const{
+		const T* closestpoint = NULL;
+		if ( points.size() > 0 ){
+			real_t mindist =  distance(*points.front(), point);
+			closestpoint = points.front();
+			for(std::vector<const T*>::const_iterator piter = points.begin() + 1; piter != points.end(); ++piter){
+				real_t curdist = distance(**piter, point);
+				closestpoint = curdist < mindist ? *piter : closestpoint;
+				mindist = std::min(mindist, curdist);
+			}
+		}
+		return closestpoint;
 	}
 
 	//input is begin and end iterators to a container of pointer of Ts
--- geomboost_test.cpp
+++ geomboost_test.cpp
@@ -1,10 +1,16 @@
 #include <geomboost.h>
 #include <vector>
 #include <iostream>
+#include <time.h>
 
 #include <map>
 #include <list>
+#include <Windows.h>
 
+#undef max
+
+
+static const float PI = 3.14159265358979323846f;
 
 struct testpoint2{
 	float xl,yl,zl;
@@ -38,35 +44,81 @@ struct testpoint{
 };
 
 std::ostream& operator<<(std::ostream& ostr, const testpoint& tp) {
-	ostr << tp.x() << ":" << tp.y() << ":" << tp.z() << std::endl;
+	ostr << tp.x() << ":" << tp.y() << ":" << tp.z();
 	return ostr;
 }
 
-
+using namespace std;
 using namespace geomboost;
+
+static void test_on_sphere(float avgradius, float thickness, int numtests){
+	std::vector<testpoint> vp;
+	srand ((unsigned int)5);
+
+	const float numpoints = 1000000.0f;
+	
+	float jjincr = PI * sqrt(1.0f/numpoints);
+	float iiincr = 2*PI * sqrt(1.0f/numpoints);
+
+	
+
+	for(float ii = 0; ii < 2*PI; ii += iiincr){
+		for (float jj = 0; jj < PI; jj += jjincr){
+			float radius = ((float)rand()/RAND_MAX - 0.5f)*thickness + avgradius;
+			float cosii = cos(ii);
+			float sinii = sin(ii);
+			float cosjj = cos(jj);
+			float sinjj = sin(jj);
+			vp.push_back(testpoint(radius*cosii*sinjj, radius*sinii*sinjj, radius*cosjj));
+		}
+	}
+	closest_point_finder<testpoint> cpf(vp.begin(), vp.end()); 
+	
+	float totaltime1 = 0;
+	float totaltime2 = 0;
+	for(int ii = 0; ii < numtests; ++ii)
+	{
+		testpoint tp(((float)rand()/RAND_MAX - 0.5f)*thickness*1.5f + avgradius, 
+					((float)rand()/RAND_MAX - 0.5f)*thickness*1.5f + avgradius, 
+					((float)rand()/RAND_MAX - 0.5f)*thickness*1.5f + avgradius);
+		//testpoint tp((float)(8+rand()%4), (float)(8+rand()%4), (float)(8+rand()%4));
+		DWORD t1, t2, t3;
+		t1 = GetTickCount();
+		const testpoint* cp1 = cpf.find_closest(tp);
+		t2 = GetTickCount();
+		const testpoint* cp2 = cpf.find_closest_linear(tp);
+		t3 = GetTickCount();
+		totaltime1 += (t2 -t1);
+		totaltime2 += (t3 -t2);
+		float d1 = geomboost::distance(*cp1, tp);
+		float d2 = geomboost::distance(*cp2, tp);
+		if( fabs(d1 - d2) > 0)
+		{	
+			cout << " AT " << ii << " dist1 = " << geomboost::distance(*cp1, tp) << ":" << " TIME = " << t2-t1;
+			cout << " dist2 = " << geomboost::distance(*cp2, tp) << ":" << " TIME = " << t3-t2 << std::endl;			;
+			cout << " TEST POINT = " << tp << " D1 = " << *cp1 << " D2 = " << *cp2 << endl;			
+		}
+		if (ii % 10 == 0){
+			cout << " AVG TIME1 = " << totaltime1/(ii+1) << "TIME 2 " << totaltime2/(ii+1) << endl;
+		}
+	}
+
+	cout << " AVG TIME1 = " << totaltime1/numtests << "TIME 2 " << totaltime2/numtests << endl;
+}
 
 
 
 void tester_pod_xyz(){
 
-	std::vector<testpoint> vp;
+	
 	std::map<int, testpoint> mp;
 	std::map<testpoint, int> mp2;
 	std::list<const testpoint*> lpp;
 	std::list<testpoint*> lpp2;
 	std::list<const testpoint> lpp3;
 
-	float a = 5,b = 10,c = 5;
-
-	const unsigned int ii = 10;
 	
-
-	unsigned kk = std::max((unsigned int)((a-b)/c), ii);
-
-	testpoint pps[10];
-	for(float ii = -5; ii < 5; ++ii){
-		vp.push_back(testpoint(ii,ii+1,ii-1));
-	}
+	/*
 
 	closest_point_finder<testpoint> cpf(vp.begin(), vp.end());
 
@@ -87,7 +139,7 @@ void tester_pod_xyz(){
 	const testpoint* cp7 = cpf7.find_closest(pps[3]);
 
 
-	if (cp)	std::cout << *cp;
+	if (cp)	std::cout << *cp << " dist " << distance(*cp, pps[3]) << std::endl;
 	if (cp1)	std::cout << *cp1;
 	if (cp3)	std::cout << *cp3;
 	if (cp4)	std::cout << *cp4;
@@ -102,13 +154,16 @@ void tester_pod_xyz(){
 	boundingbox<testpoint> bb3(lpp3.begin(), lpp3.end());
 
 	testpoint t1, t2;
-	testpoint t = t1 + t2;
+	testpoint t = t1 + t2;*/
 
 	
 
-}
+
+} 
 
 int main(){
-	tester_pod_xyz();
+	test_on_sphere(5, 2, 100);
 	return 0;
+
+	 
 }
